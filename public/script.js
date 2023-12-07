5b62bba3ed99204e5d929bcd7f5aaa5566f1b771
Tin Nguyen <nguyentritin211@gmail.com>
2023-12-07 21:30:50 +0200

    Update test

--- public/script.js
+++ public/script.js
@@ -2,15 +2,20 @@ function getAndUpdate(){
     console.log("Updating List...");
     tit = document.getElementById('title').value;
     desc = document.getElementById('description').value;
+    priority = document.getElementById('priority').value;
+
+    document.getElementById('title').value = "";
+    document.getElementById('description').value = "";
+
     if (localStorage.getItem('itemsJson')==null){
         itemJsonArray = [];
-        itemJsonArray.push([tit, desc]);
+        itemJsonArray.push([tit, desc, priority]);
         localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
     }
     else{
         itemJsonArrayStr = localStorage.getItem('itemsJson')
         itemJsonArray = JSON.parse(itemJsonArrayStr);
-        itemJsonArray.push([tit, desc]);
+        itemJsonArray.push([tit, desc, priority]);
         localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
     }
     update();
@@ -29,16 +34,31 @@ function update(){
     let tableBody = document.getElementById("tableBody");
     let str = "";
     itemJsonArray.forEach((element, index) => {
+        let priorityColor = getColorByPriority(element[2]);
         str += `
         <tr>
         <th scope="row">${index + 1}</th>
         <td>${element[0]}</td>
         <td>${element[1]}</td> 
+        <td style="background-color: ${priorityColor}; color: #000;">${element[2]}</td>
         <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})">Delete</button></td> 
         <td><button class="btn btn-sm btn-primary" onclick="edit(${index})">Edit</button></td> 
         </tr>`; 
     });
     tableBody.innerHTML = str;
+}
+
+function getColorByPriority(priority) {
+    switch (priority) {
+        case 'low':
+            return '#c6e5c6';
+        case 'medium':
+            return '#e0e0b3';
+        case 'high':
+            return '#e0b3b3';
+        default:
+            return 'white';
+    }
 }
 
 add = document.getElementById("add");
@@ -65,6 +85,35 @@ function edit(itemIndex) {
     editItem(itemIndex, newTitle, newDescription);
 }
 
+function edit(itemIndex) {
+    // Show the edit pop-up
+    document.getElementById("editPopup").style.display = "block";
+
+    // Set up the confirmEdit function with the itemIndex
+    window.confirmEdit = function () {
+        let newTitle = document.getElementById("newTitle").value;
+        let newDescription = document.getElementById("newDescription").value;
+
+        // Call the edit function with the new data
+        editItem(itemIndex, newTitle, newDescription);
+
+        // Hide the edit pop-up
+        closeEditPopup();
+    };
+
+    // Set up the closeEditPopup function
+    window.closeEditPopup = function () {
+        document.getElementById("editPopup").style.display = "none";
+        // Remove the confirmEdit and closeEditPopup functions to avoid conflicts
+        delete window.confirmEdit;
+        delete window.closeEditPopup;
+    };
+
+    document.getElementById('newTitle').value = "";
+    document.getElementById('newDescription').value = "";
+
+}
+
 function editItem(itemIndex, newTitle, newDescription) {
     console.log("Edit", itemIndex, newTitle, newDescription);
     itemJsonArrayStr = localStorage.getItem('itemsJson');
@@ -78,10 +127,23 @@ function editItem(itemIndex, newTitle, newDescription) {
     update();
 }
 
-function clearStorage(){
-    if (confirm("Do you really want to clear?")){
-        console.log('Clearing the storage');
-        localStorage.clear();
-        update();
-    }
+
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
 }
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+document.getElementById("clear").addEventListener("click", openPopup);
+
+document.getElementById("closePopup").addEventListener("click", closePopup);
+
+document.getElementById("confirmClear").addEventListener("click", function () {
+    console.log('Clearing the storage');
+    localStorage.clear();
+    update(); 
+    closePopup();
+    
+});
